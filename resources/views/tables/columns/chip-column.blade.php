b07db9054be5d2b21ff51cf9b40efc1625d4f933
Bezhan Salleh <bezhan_salleh@yahoo.com>
2022-08-08 14:20:13 +0430

    ChipColumn

--- resources/views/tables/columns/chip-column.blade.php
+++ resources/views/tables/columns/chip-column.blade.php
@@ -1,102 +1,94 @@
 @php
-$state = $getFormattedState();
+    $state = $getFormattedState();
 
-$stateColor = match ($getStateColor()) {
-    'primary' => \Illuminate\Support\Arr::toCssClasses([
-        'bg-primary-600 text-white' => $getInverse(),
-        'text-primary-600 bg-gray-500/10' => !$getInverse(),
-        'dark:text-primary-500 dark:bg-primary-500/10' => !$getInverse() && config('tables.dark_mode'),
-    ]),
-    'success' => \Illuminate\Support\Arr::toCssClasses([
-        'bg-success-600 text-white' => $getInverse(),
-        'text-success-600 bg-gray-500/10' => !$getInverse(),
-        'dark:text-success-500 dark:bg-success-500/10' => !$getInverse() && config('tables.dark_mode'),
-    ]),
-    'warning' => \Illuminate\Support\Arr::toCssClasses([
-        'bg-warning-600 text-white' => $getInverse(),
-        'text-warning-600 bg-gray-500/10' => !$getInverse(),
-        'dark:text-warning-500 dark:bg-warning-500/10' => !$getInverse() && config('tables.dark_mode'),
-    ]),
-    'danger' => \Illuminate\Support\Arr::toCssClasses([
-        'bg-danger-600 text-white' => $getInverse(),
-        'text-danger-600 bg-gray-500/10' => !$getInverse(),
-        'dark:text-danger-500 dark:bg-danger-500/10' => !$getInverse() && config('tables.dark_mode'),
-    ]),
-    null => \Illuminate\Support\Arr::toCssClasses(['text-gray-600 bg-gray-500/10', 'dark:text-gray-300 dark:bg-gray-500/20' => config('tables.dark_mode')]),
-    default => $getStateColor()
-};
+    $stateColor = match ($getStateColor()) {
+        'primary' => \Illuminate\Support\Arr::toCssClasses([
+            'bg-primary-600 text-white' => $getInvert(),
+            'text-primary-600 bg-primary-500/10' => !$getInvert(),
+            'dark:text-primary-500 dark:bg-primary-500/10' => !$getInvert() && config('tables.dark_mode'),
+        ]),
+        'success' => \Illuminate\Support\Arr::toCssClasses([
+            'bg-success-600 text-white' => $getInvert(),
+            'text-success-600 bg-success-500/10' => !$getInvert(),
+            'dark:text-success-500 dark:bg-success-500/10' => !$getInvert() && config('tables.dark_mode'),
+        ]),
+        'warning' => \Illuminate\Support\Arr::toCssClasses([
+            'bg-warning-600 text-white' => $getInvert(),
+            'text-warning-600 bg-warning-500/10' => !$getInvert(),
+            'dark:text-warning-500 dark:bg-warning-500/10' => !$getInvert() && config('tables.dark_mode'),
+        ]),
+        'danger' => \Illuminate\Support\Arr::toCssClasses([
+            'bg-danger-600 text-white' => $getInvert(),
+            'text-danger-600 bg-danger-500/10' => !$getInvert(),
+            'dark:text-danger-500 dark:bg-danger-500/10' => !$getInvert() && config('tables.dark_mode'),
+        ]),
+        null => \Illuminate\Support\Arr::toCssClasses(['text-gray-600 bg-gray-500/10', 'dark:text-gray-300 dark:bg-gray-500/20' => config('tables.dark_mode')]),
+        default => $getStateColor()
+    };
 
-$iconColor = match ($getStateColor()) {
-    'primary' => \Illuminate\Support\Arr::toCssClasses([
-        'text-white bg-primary-600 ' => !$getInverse(),
-        'text-primary-600 bg-white' => $getInverse(),
-        'dark:text-gray-100' => !$getInverse() && config('tables.dark_mode'),
-    ]),
-    'success' => \Illuminate\Support\Arr::toCssClasses([
-        'text-white bg-success-600' => !$getInverse(),
-        'text-success-600 bg-white' => $getInverse(),
-        'dark:text-gray-100' => !$getInverse() && config('tables.dark_mode'),
-    ]),
-    'warning' => \Illuminate\Support\Arr::toCssClasses([
-        'text-white bg-warning-600 ' => !$getInverse(),
-        'text-warning-600 bg-white' => $getInverse(),
-        'dark:text-gray-100' => !$getInverse() && config('tables.dark_mode'),
-    ]),
-    'danger' => \Illuminate\Support\Arr::toCssClasses([
-        'text-white bg-danger-600' => !$getInverse(),
-        'text-danger-600 bg-white' => $getInverse(),
-        'dark:text-gray-100' => !$getInverse() && config('tables.dark_mode'),
-    ]),
-    null => \Illuminate\Support\Arr::toCssClasses(['text-white bg-gray-600', 'dark:text-gray-100 dark:bg-gray-800' => config('tables.dark_mode')]),
-    default => $getStateColor()
-};
+    $iconColor = match ($getStateColor()) {
+        'primary' => \Illuminate\Support\Arr::toCssClasses([
+            'text-white bg-primary-600 ' => !$getInvert(),
+            'text-primary-600 bg-white' => $getInvert(),
+            'dark:text-gray-100' => !$getInvert() && config('tables.dark_mode'),
+        ]),
+        'success' => \Illuminate\Support\Arr::toCssClasses([
+            'text-white bg-success-600' => !$getInvert(),
+            'text-success-600 bg-white' => $getInvert(),
+            'dark:text-gray-100' => !$getInvert() && config('tables.dark_mode'),
+        ]),
+        'warning' => \Illuminate\Support\Arr::toCssClasses([
+            'text-white bg-warning-600 ' => !$getInvert(),
+            'text-warning-600 bg-white' => $getInvert(),
+            'dark:text-gray-100' => !$getInvert() && config('tables.dark_mode'),
+        ]),
+        'danger' => \Illuminate\Support\Arr::toCssClasses([
+            'text-white bg-danger-600' => !$getInvert(),
+            'text-danger-600 bg-white' => $getInvert(),
+            'dark:text-gray-100' => !$getInvert() && config('tables.dark_mode'),
+        ]),
+        null => \Illuminate\Support\Arr::toCssClasses(['text-white bg-gray-600', 'dark:text-gray-100 dark:bg-gray-800' => config('tables.dark_mode')]),
+        default => $getStateColor()
+    };
 
-$stateIcon = $getStateIcon();
-$iconPosition = $getIconPosition();
-$avatar = $getAvatar();
-
+    $stateIcon = $getStateIcon();
+    $iconPosition = $getIconPosition();
+    $iconClasses = 'h-4 w-4';
 @endphp
 
 <div
     {{ $attributes->merge($getExtraAttributes())->class([
-            'px-4 py-3 flex filament-tables-chip-column',
+            'px-4 py-3 flex filament-addons-badge-column',
             match ($getAlignment()) {
                 'left' => 'justify-start',
                 'center' => 'justify-center',
                 'right' => 'justify-end',
                 default => null
             },
-        ])
-    }}
->
+        ]) }}>
     @if (filled($state))
         <div @class([
-            'inline-flex items-center justify-center min-h-6 px-2 py-0.5 text-sm font-medium tracking-tight rounded-xl whitespace-normal',
+            'inline-flex justify-center items-center px-[10px] text-sm rounded-full font-medium tracking-tight',
             $stateColor => $stateColor,
         ])>
-            @if ($avatar)
-            <div class="inline-flex !-ml-[0.36rem] mr-1 rtl:!-mr-[0.36rem] rtl:!ml-1 rounded-full h-5 w-5 ">
-                <img src="{{ $getAvatarPath() }}" class="object-cover object-center rounded-full" />
+            <div @class([
+                'flex justify-between items-center min-h-6 min-w-6',
+                'flex-row-reverse' => $stateIcon && $iconPosition === 'before',
+            ])>
+
+                <span>{{ $state }}</span>
+
+                @if ($stateIcon)
+                    <div @class([
+                        'flex justify-center items-center whitespace-nowrap text-center rounded-full h-5 w-5 my-0.5',
+                        '-translate-x-2 rtl:translate-x-2' => $iconPosition === 'before',
+                        'translate-x-2 rtl:-translate-x-2' => $iconPosition === 'after',
+                        $iconColor => $iconColor,
+                    ])>
+                        <x-dynamic-component :component="$stateIcon" :class="$iconClasses" />
+                    </div>
+                @endif
             </div>
-            @endif
-
-            @if ($stateIcon && $iconPosition === 'before' && !$avatar)
-                <div
-                    class="inline-flex items-center justify-center !-ml-[0.36rem] !mr-1 rtl:!-mr-[0.36rem] rtl:!ml-1 rounded-full h-5 w-5 text-center {{ $iconColor }}">
-                    <x-dynamic-component :component="$stateIcon" class="h-3 w-3" />
-                </div>
-            @endif
-
-            <span>
-                {{ $state }}
-            </span>
-
-            @if ($stateIcon && $iconPosition === 'after' && !$avatar)
-                <div
-                    class="inline-flex items-center justify-center !-mr-[0.36rem] !ml-1 rtl:!mr-1 rtl:!-ml-[0.36rem] rounded-full h-5 w-5 text-center {{ $iconColor }}">
-                    <x-dynamic-component :component="$stateIcon" class="h-3 w-3" />
-                </div>
-            @endif
         </div>
     @endif
 </div>
